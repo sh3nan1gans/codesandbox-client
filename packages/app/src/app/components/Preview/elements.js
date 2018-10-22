7b54145106415ebb2d2dfb0d6287141f860c6e06
Bogdan Luca <luca.bogdan@gmail.com>
2018-10-22 22:10:21 +0300

    SSE bug fixes & improvements.

--- packages/app/src/app/components/Preview/elements.js
+++ packages/app/src/app/components/Preview/elements.js
@@ -30,8 +30,6 @@ export const Loading = styled.div`
   bottom: 0;
   right: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
   background-color: rgba(0, 0, 0, 0.75);
   padding: 2rem;
   display: flex;
